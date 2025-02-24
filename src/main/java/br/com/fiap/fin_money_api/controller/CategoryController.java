826cd274010e4a3e83c73f5be1343838d01f1d0b
Marsol4 <enzogmarsola@gmail.com>
2025-02-24 11:55:13 -0300

    Cria a Category.java

--- src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
@@ -4,13 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fiap.fin_money_api.model.Category;
+
 @RestController
 public class CategoryController {
 
 
     @RequestMapping(produces = "application/json", path = "/categories", method = {RequestMethod.GET})
-    public String index(){
-        return "ok";
+    public Category index(){
+        return """
+            {
+                "name": "Educação"
+            }
+        """;
     }
 
 }
