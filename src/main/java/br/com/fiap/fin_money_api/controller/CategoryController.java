8224fcfadbb5825a254af98a242e11022cba78aa
MarsoL4 <enzogmarsola@gmail.com>
2025-02-27 11:28:44 -0300

    atualiza o metodo principal do postmapping para o tipo Category

--- src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
@@ -3,9 +3,11 @@ package br.com.fiap.fin_money_api.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.fin_money_api.model.Category;
@@ -21,9 +23,11 @@ public class CategoryController {
     }
 
     @PostMapping("/categories")
-    public void create(@RequestBody Category category){
+    @ResponseStatus(code = HttpStatus.CREATED) //Status que será passado em caso de sucesso ao Postman
+    public Category create(@RequestBody Category category){
         System.out.println("Cadastrando categoria " + category.getName());
         repository.add(category);
+        return category;
     }
 
 
