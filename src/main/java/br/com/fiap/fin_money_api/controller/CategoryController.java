ccbe5047416b5c789b125299acc95647673f80ce
MarsoL4 <enzogmarsola@gmail.com>
2025-02-27 11:13:53 -0300

    Adiciona um @RequestBody nos parametros do metodo do PostMapping

--- src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
@@ -2,6 +2,7 @@ package br.com.fiap.fin_money_api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.fin_money_api.model.Category;
@@ -9,16 +10,15 @@ import br.com.fiap.fin_money_api.model.Category;
 @RestController
 public class CategoryController {
 
-
     @GetMapping(path = "/categories")
     public Category index(){
         return new Category(1L, "Educação", "book");
     }
 
     @PostMapping("/categories")
-    public void create(){
-        System.out.println("Cadastrando categoria");
-
+    public void create(@RequestBody Category category){
+        System.out.println("Cadastrando categoria " + category.getName());
     }
+
 
 }
