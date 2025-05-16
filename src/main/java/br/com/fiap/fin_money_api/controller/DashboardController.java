9c2f92c8ab230dfdcc79dad58014d9d7380d4a44
Enzo Marsola <enzogmarsola@gmail.com>
2025-05-16 12:08:57 -0300

    Added Spring AI dependencies and configuration for OpenAI integration

---
+++ src/main/java/br/com/fiap/fin_money_api/controller/DashboardController.java
@@ -0,0 +1,23 @@
+package br.com.fiap.fin_money_api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.fin_money_api.service.AiAnaliseService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class DashboardController {
+    
+    @Autowired
+    private AiAnaliseService service;
+
+    @GetMapping("dashboard")
+    public String analise(@RequestParam String lang) {
+        return service.getExpenseAnalise(lang);
+    }
+    
+}
--- src/main/java/br/com/fiap/fin_money_api/repository/TransactionRepository.java
+++ src/main/java/br/com/fiap/fin_money_api/repository/TransactionRepository.java
@@ -1,16 +1,21 @@
 package br.com.fiap.fin_money_api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import br.com.fiap.fin_money_api.model.Transaction;
+import br.com.fiap.fin_money_api.model.TransactionType;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long>, JpaSpecificationExecutor<Transaction>{
+
+    List<Transaction> findByType(TransactionType type);
+
     //@Query("SELECT t FROM TRANSACTION as t WHERE t.description = : description") //JPQL
     // Page<Transaction> findByDescriptionContainingIgnoringCase(String description, Pageable pageable); //Query methods
 
-    // Page<Transaction> findByDescriptionContainingIgnoringCaseAndDate(String description, LocalDate date,
-    //         Pageable pageable);
+    // Page<Transaction> findByDescriptionContainingIgnoringCaseAndDate(String description, LocalDate date, Pageable pageable);
 
     // Page<Transaction> findByDate(LocalDate date, Pageable pageable);
     
